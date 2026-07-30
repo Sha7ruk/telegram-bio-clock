bfe53e1df8daddfc9ad816a083a96226c45967ad
Sha7ruk <sharuk7m@gmail.com>
2026-07-31 01:31:59 +0200

    Update bio.py

--- bio.py
+++ bio.py
@@ -4,13 +4,14 @@ from datetime import datetime
 from zoneinfo import ZoneInfo
 
 from telethon import TelegramClient
+from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
 
 api_id = int(os.environ["API_ID"])
 api_hash = os.environ["API_HASH"]
-session = os.environ.get("SESSION_NAME", "bio")
+session = os.environ["SESSION_STRING"]
 
-client = TelegramClient(session, api_id, api_hash)
+client = TelegramClient(StringSession(session), api_id, api_hash)
 
 async def main():
     await client.start()
@@ -21,7 +22,6 @@ async def main():
     bio = f"🕒 {now.strftime('%H:%M')}"
 
     await client(UpdateProfileRequest(about=bio))
-    print("Bio updated:", bio)
 
     await client.disconnect()
 
